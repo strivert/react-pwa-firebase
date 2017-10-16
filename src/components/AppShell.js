375eb0896d03916243d42d1766a36abeeb8cd5cf
prageer <audrius88716@mail.com>
2017-10-17 02:48:51 +0300

    6-material ui layout

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -3,12 +3,33 @@ import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
 import { AppBar, Drawer, MenuItem } from 'material-ui';
 
 class AppShell extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			open: false
+		};
+	}
+
+	handleDrawerToggle = () => {
+		this.setState({
+			open: !this.state.open
+		});
+	}
+
+	handleRequestChange = (open) => {
+		this.setState({
+			open: open
+		});
+	}
+
  	render() {
  		return (
  			<MuiThemeProvider>
 		 		<div>
-		 			<AppBar />
-		 			<Drawer>
+		 			<AppBar onLeftIconButtonTouchTap={this.handleDrawerToggle} />
+		 			<Drawer open={this.state.open} docked={false}
+		 				onRequestChange={this.handleRequestChange}>
 		 				<MenuItem primaryText={'Home'} />
 		 			</Drawer>
 		 			<div id="content">
